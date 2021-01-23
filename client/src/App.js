96dac2d28992b9762b2d22ad7c85929335f1e051
ericw142 <66480663+ericw142@users.noreply.github.com>
2021-01-23 12:25:22 -0500

    initial react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Drawing from "./pages/drawing";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Profile from "./pages/profile";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Home} />
+        <Route exact path="/drawing" component={Drawing} />
+        <Route exact path="/profile" component={Profile} />
+        <Route path="/login" component={Login} />
+      </div>
+    </Router>
   );
 }
 
