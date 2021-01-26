50761486913c010c63727e7a7ed36eee3701ad7a
ericw142 <66480663+ericw142@users.noreply.github.com>
2021-01-25 21:35:40 -0500

    login page is reactive

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -2,6 +2,18 @@ import React, { Component } from "react";
 import API from "../utils/API";
 
 class SignupForm extends Component {
+    state = {
+        signedUp: false
+    }
+
+    successDisplay = () => {
+        if (this.state.signedUp) {
+            return <p>You have successfully signed up! Please use the login above.</p>;
+        }
+        else {
+            return;
+        }
+    }
 
     submitSignupForm = event => {
         event.preventDefault();
@@ -11,27 +23,30 @@ class SignupForm extends Component {
         let password = event.target.password.value
 
         API.signup({username, email, password})
-        .then(res => console.log(res))
+        .then(res => this.setState({signedUp: true}))
         .catch(err => console.log(err));
     };
 
     render() {
         return(
-            <form className="signup" onSubmit={this.submitSignupForm}>
-                <div className="form-group">
-                    <label htmlFor="username">Username</label>
-                    <input type="text" name="username" className="form-control" placeholder="Username"></input>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="email">Email</label>
-                    <input type="email" name="email" className="form-control" placeholder="Email"></input>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="password">Password</label>
-                    <input type="password" name="password" className="form-control" placeholder="Password"></input>
-                </div>
-                <button type="submit" className="btn btn-default">Register</button>
-            </form>
+            <div>
+                 <form className="signup" onSubmit={this.submitSignupForm}>
+                    <div className="form-group">
+                        <label htmlFor="username">Username</label>
+                        <input type="text" name="username" className="form-control" placeholder="Username"></input>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" className="form-control" placeholder="Email"></input>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" className="form-control" placeholder="Password"></input>
+                    </div>
+                    <button type="submit" className="btn btn-default">Register</button>
+                 </form>
+                 {this.successDisplay()}
+            </div>
         )
     }
 }
